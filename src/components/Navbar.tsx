39ef74f152fb58549fb9da9e1b4c18ef8adbdaea
Fernando Guerrero Ordoñez <138276738+WarriorGW@users.noreply.github.com>
2025-06-19 17:41:41 -0600

    Modified organization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
-import React from "react";
 import { buttonVariants } from "./ui/button";
 
 function Navbar() {
   return (
-    <nav className="w-full h-[7vh] bg-primary flex flex-row justify-between items-center px-4">
+    <nav className="w-full h-[7vh] bg-amber-700 flex flex-row justify-between items-center px-4">
       <Link href="/" className={buttonVariants({ variant: "ghost" })}>
-        Inicio
+        Shelver
       </Link>
+      <div className="flex flex-row gap-4">
+        <p>Nombre de usuario</p>
+        <p>Rol de usuario</p>
+      </div>
       <div>
         <Link href="/register" className={buttonVariants({ variant: "ghost" })}>
           Registrarse
