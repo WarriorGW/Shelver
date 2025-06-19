a9e0f7598e1fb335536c46a257396c8493c72a50
Fernando Guerrero Ordoñez <138276738+WarriorGW@users.noreply.github.com>
2025-06-19 17:13:29 -0600

    Feature: Dashboard first implementation

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import BodyWrapper from "@/components/BodyWrapper";
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <BodyWrapper>{children}</BodyWrapper>;
+}
+
+export default DashboardLayout;
