5cc8ba9b6e0c6f6f35caa5df95105e771cfb6e7a
Fernando Guerrero Ordoñez <138276738+WarriorGW@users.noreply.github.com>
2025-06-26 23:46:08 -0600

    Feature: Beta user menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,57 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutDashboard, LogOut, Settings, User, Users } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 
 function Navbar() {
   return (
-    <nav className="w-full h-[7vh] bg-amber-700 flex flex-row justify-between items-center px-4">
+    <nav className="w-full h-[7vh] bg-sky-200 flex flex-row justify-between items-center px-4">
       <Link href="/" className={buttonVariants({ variant: "ghost" })}>
         Shelver
       </Link>
-      <div className="flex flex-row gap-4">
-        <p>Nombre de usuario</p>
-        <p>Rol de usuario</p>
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className={buttonVariants({ variant: "ghost" })}>
+            :user:
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem asChild>
+              <Link href="/profile">
+                <User /> Perfil
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href="/settings">
+                <Settings /> Ajustes
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger className="[&_svg:not([class*='text-'])]:text-muted-foreground gap-2 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+                  <LayoutDashboard /> Dashboard
+                </DropdownMenuSubTrigger>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/users">
+                      <Users /> Usuarios
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuSub>
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut /> Cerrar sesion
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       <div>
         <Link href="/register" className={buttonVariants({ variant: "ghost" })}>
